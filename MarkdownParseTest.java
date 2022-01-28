dc3cb0deadd30ac475886cccfadc290f26a260f5
Prash Katukojwala <31807747+prashasthk@users.noreply.github.com>
2022-01-27 16:39:18 -0800

    Wrote first JUnit test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,23 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFile1() throws IOException {
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        assertEquals(expected, MarkdownParse.getLinks(contents));
     }
 }
